c88c284fa952690ea832c8d93ca979de8df4e4c0
wangyunxiu <020142103>
2026-04-10 16:53:09 +0800

    媒体详情

---
+++ lib/business/media_detail/media_chapter_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MediaChapterList extends StatefulWidget {
+  const MediaChapterList({Key? key}) : super(key: key);
+
+  @override
+  _MediaChapterListState createState() => _MediaChapterListState();
+}
+
+class _MediaChapterListState extends State<MediaChapterList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
